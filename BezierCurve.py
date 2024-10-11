1cd109d3f0a9721b7441ab0f7e73272b877ea9e9
jp05451 <59051826+jp05451@users.noreply.github.com>
2024-10-11 17:38:34 +0800

    add Point object

--- BezierCurve.py
+++ BezierCurve.py
@@ -1,23 +1,41 @@
 from turtle import Screen, Turtle
 
+
+class Point:
+    def __init__(self, x, y):
+        self.point = Turtle(shape="square")
+        self.point.penup()
+        self.point.goto(x, y)
+        self.point.shapesize(0.5, 0.5)
+        self.position = (x, y)
+
+
 class BezierCurve:
     def __init__(self, points):
         self.order = len(points) - 1
         self.screen = Screen()
         self.screen.setup(width=800, height=600)
-        self.points = [Turtle(shape="circle") for _ in range(4)]
+        self.points=[]
         # 創建四個點
-        self.positions = [(-100, 100), (100, 100), (-100, -100), (100, -100)]
+        for p in points:
+            temp=Point(p[0],p[1])
+            # 綁定拖曳事件
+            temp.point.ondrag(lambda x, y, p=temp.point: self.on_drag(x, y, p))
+            # storge object Point
+            self.points.append(temp)
+            
+        # self.positions = [(-100, 100), (100, 100), (-100, -100), (100, -100)]
 
         # 設置點的初始位置
-        for point, pos in zip(self.points, self.positions):
-            point.penup()
-            point.goto(pos)
-            point.shapesize(0.5, 0.5)
-            # 綁定拖曳事件
-            point.ondrag(lambda x, y, p=point: self.on_drag(x, y, p))
+        # for point in  points:
+        #     point.penup()
+        #     point.goto(pos)
+        #     point.shapesize(0.5, 0.5)
+        #     # 綁定拖曳事件
+        #     point.ondrag(lambda x, y, p=point: self.on_drag(x, y, p))
 
     def on_drag(self, x, y, point):
+        print(f"drag: {x},{y}")
         point.ondrag(None)  # 防止遞歸調用
         point.goto(x, y)
         point.ondrag(lambda x, y: self.on_drag(x, y, point))
